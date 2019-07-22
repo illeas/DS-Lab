49d2e4f9ae1961111fe997b5893934d1d8f613aa
Illeas <illeashossain34@gmail.com>
2019-07-22 19:39:58 +0600

    update

--- D/main.cpp
+++ D/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -6,18 +6,25 @@ int main()
 {
     int t;
     cin >> t;
-    for(int p=1;p<=t; p++){
+    for(int p=1; p<=t; p++)
+    {
         int n, th=0, tl=0;
         cin >> n;
         int a[n];
-        for(int i=0;i<n;i++){
+        for(int i=0; i<n; i++)
+        {
             cin >> a[i];
         }
-        if(n>1){
-            for(int j=0;j<n-1;j++){
-                if(a[j]<a[j+1]){
+        if(n>1)
+        {
+            for(int j=0; j<n-1; j++)
+            {
+                if(a[j]<a[j+1])
+                {
                     th++;
-                }else if(a[j]>a[j+1]){
+                }
+                else if(a[j]>a[j+1])
+                {
                     tl++;
                 }
             }
