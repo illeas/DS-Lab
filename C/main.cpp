445f1ef45168c105de2905dcd6b86e174e42acac
Illeas <illeashossain34@gmail.com>
2019-07-22 19:38:03 +0600

    Update main.cpp

--- C/main.cpp
+++ C/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -6,13 +6,19 @@ int main()
 {
     int t, a, b, c;
     cin >> t;
-    for(int i=1;i<=t;i++){
+    for(int i=1; i<=t; i++)
+    {
         cin >> a >> b >> c;
-        if(((a>b) && (c>a)) || ((a<b)&&(a>c))){
+        if(((a>b) && (c>a)) || ((a<b)&&(a>c)))
+        {
             cout << "Case " << i << ": " << a;
-        }else if(((b>a)&&(b<c)) || ((b<a)&&(b>c))){
+        }
+        else if(((b>a)&&(b<c)) || ((b<a)&&(b>c)))
+        {
             cout << "Case " << i << ": " << b;
-        }else if(((c>a)&&(c<b)) || ((c<a)&&(c>b))){
+        }
+        else if(((c>a)&&(c<b)) || ((c<a)&&(c>b)))
+        {
             cout << "Case " << i << ": " << c;
         }
         cout << endl;
